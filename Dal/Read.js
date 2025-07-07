f26308acf2a29b7e8ca8c636a8c380401d617175
Moti <shalom7950@gmail.com>
2025-07-07 16:48:09 +0300

    Finish Read Function

---
+++ Dal/Read.js
@@ -0,0 +1,22 @@
+import { readFile,writeFile } from "node:fs/promises"
+
+
+async function Read(path)
+{
+    //Get only string file (.txt)
+    try
+    {
+        console.log("from Read func in try block");
+        const asyncRead = await readFile(path, "utf8")
+        //check if neet convert to Json
+        return asyncRead;
+    }
+    catch (err)
+    {
+        console.log("Error from Read func in catch block!");
+        return err;
+    }
+}
+// Function check
+// const checkRead = await Read("../.././DBServer/DB/TestingFile.json")
+// console.log(checkRead);
