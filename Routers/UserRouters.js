d7afd47c6bdcd63d5c19be8684ab1b67717b80b1
Moti7950 <shalom7950@gmail.com>
2025-07-20 09:33:36 +0300

    sucsess conect to DB

--- Routers/UserRouters.js
+++ Routers/UserRouters.js
@@ -10,27 +10,7 @@ UserRouters.delete("/delete/:id", (req, res) => res.send("Delete user! \n"))
 
 UserRouters.get("/show", (req, res) => res.send("Get all players \n"))
 
-UserRouters.post("/create", async (req, res) => 
-{
-    try {
-        const { name, TimeStatistics } = req.body;
-
-        if (typeof name !== "string" || typeof TimeStatistics !== "number") {
-            return res.status(400).json({ error: "Invalid or missing parameters 😢" });
-        }
-        
-
-        const newUserForGame = await newUser(UsersDBPath ,req.body.name,)
-        await userCreate(UsersDBPath, newUserForGame);
-
-        res.status(201).json({ message: "User created successfully 😏", user: newUserForGame });
-
-    } catch (err)
-    {
-        console.error("❌ Error creating user:", err);
-        res.status(500).json({ error: "Internal server error 😢 !!" });
-    }
-});
+UserRouters.post("/create", async (req, res) =>{});
 
 UserRouters.put("/api/players/delete/update/:id", async (req, res) => { })
 
